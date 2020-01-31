94a89b128b8f448b8fba4392cfc04b28a13641b4
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-01-30 19:13:46 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mosh_Intermediate
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+## Mosh C# Tutorial Intermediate
+
