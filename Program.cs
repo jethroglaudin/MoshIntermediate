d79d6c099b4b389416f62e21cb166e45f4192cca
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-01-31 23:13:30 -0500

    updated Program.cs

--- Program.cs
+++ Program.cs
@@ -2,32 +2,12 @@
 
 namespace Mosh_Intermediate
 {
-    public class Person
-    {
-        public string Name;
-
-        public void Introduce(string to)
-        {
-            Console.WriteLine("Hi {0}, I am {1}", to, Name);
-        }
-
-        public static Person Parse(string str)
-        {
-            var person = new Person();
-            person.Name = str;
-            return person;
-        }
-    }
-
     class MainClass
     {
         public static void Main(string[] args)
         {
-            //var person = new Person();
-            var person = Person.Parse("John");
-           
-            person.Introduce("Jethro");
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello");
         }
     }
 }
