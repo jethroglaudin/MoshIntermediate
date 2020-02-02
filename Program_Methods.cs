0ea877fb059a36c6aa6857d6ffb24b19af232a64
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-02-01 22:39:57 -0500

    learning about params, out Points

---
+++ Program_Methods.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace Mosh_Intermediate
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            //var number = int.Parse("abc");
+            int number;
+            var result = int.TryParse("abc", out number);
+            if(result)
+                Console.WriteLine(number);
+            else
+                Console.WriteLine("Conversion failed");
+            
+        }
+
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 3));
+            Console.WriteLine(calculator.Add(1, 2, 3, 4));
+            Console.WriteLine(calculator.Add(new int[] { 1, 2, 3, 4, 5 }));
+        }
+
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(null);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+                point.Move(100, 200);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An unexected error occured");
+            }
+        }
+    }
+}
