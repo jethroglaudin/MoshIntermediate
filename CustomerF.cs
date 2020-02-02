923faf15358b75a2df28e2c7d34fddfca322f6a3
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-02-01 23:05:54 -0500

    Working with methods

---
+++ CustomerF.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Mosh_Intermediate
+{
+    public class CustomerF
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders = new List<Order>();
+
+        public CustomerF(int id)
+        {
+            this.Id = id;
+        }
+
+        public CustomerF(int id, string name)
+            : this(id)
+        {
+            this.Name = name;
+        }
+
+        public void Promote()
+        {
+            // 
+        }
+    }
+}
