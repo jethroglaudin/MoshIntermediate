99885404a256b08e24d0944358b0f2dfd02d3159
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-02-01 23:11:39 -0500

    added Program_Fields.cs

---
+++ Program_Fields.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Mosh_Intermediate
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            var customer = new CustomerF(1);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+
+            customer.Promote();
+
+            Console.WriteLine(customer.Orders.Count);
+            
+        }
+    }
+}
