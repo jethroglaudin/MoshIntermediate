7ae0c78bad8811c8fb59fd7a3eb7d25cb7bd7913
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-02-03 00:20:50 -0500

    created Person class to calculates age. Also uses get and set Birthdate

--- Person.cs
+++ Person.cs
@@ -4,14 +4,22 @@ namespace Mosh_Intermediate
 {
     public class Person
     {
-        public DateTime Birthdate { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public DateTime Birthdate { get; private set; }
+
+
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
 
         public int Age
         {
             get
             {
                 var timeSpan = DateTime.Today - Birthdate;
-                var years = timeSpan.Days / 365;
+                var years = timeSpan.Days/365;
 
                 return years;
             }
