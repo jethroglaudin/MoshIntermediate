cbbc0805143b4aab472e97db72aef85999cff524
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-02-05 18:23:09 -0500

    added StopWatch project

---
+++ Projects/StopWatch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace StopWatch
+{
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("STOPWATCH APP");
+            Console.WriteLine("Press Enter to start and stop the stop watch!");
+
+            var stopWatch = new StopWatch();
+
+            while (true)
+            {
+                var userInput = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(userInput))
+                {
+                    stopWatch.Start();
+                    Console.ReadLine();
+                    stopWatch.Stop();
+
+                    Console.WriteLine("Duration: {0}:{1}:{2}:{3}  ", stopWatch.GetTime().Hours, stopWatch.GetTime().Minutes, stopWatch.GetTime().Seconds, stopWatch.GetTime().Milliseconds);
+                   
+                    continue;
+                        
+                }
+
+                break;
+            }
+
+            
+        }
+    }
+}
---
+++ Projects/StopWatch/StopWatch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+namespace StopWatch
+{
+    public class StopWatch
+    {
+        private DateTime _startTime;
+        private DateTime _stopTime;
+        private TimeSpan _duration;
+        private bool isRunning;
+
+        public void Start()
+        {
+            try
+            {
+                Console.WriteLine("Timer is running");
+
+                _startTime = DateTime.UtcNow;
+                isRunning = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Stopwatch is already running", ex);
+            }
+        }
+
+        public void Stop()
+        {
+            try
+            {
+                _stopTime = DateTime.UtcNow;
+                isRunning = false;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Stopwatch is not running", ex);
+            }
+        }
+
+        public TimeSpan GetTime()
+        {
+            _duration = _stopTime - _startTime;
+            return _duration;
+        }
+    }
+
+   
+
+    
+}
