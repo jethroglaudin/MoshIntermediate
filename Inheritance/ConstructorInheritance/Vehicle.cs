fadf16ce86abf8f1b42db570a02d3950f925257f
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-02-10 22:41:32 -0500

    added Vehicle class - pratice with base constructors

---
+++ Inheritance/ConstructorInheritance/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConstructorInheritance
+{
+    public class Vehicle
+    {
+        private readonly string _registrationNumber;
+
+        //public Vehicle()
+        //{
+        //    Console.WriteLine("Vehicle is being initialized");
+        //}
+
+        public Vehicle(string registrationNumber)
+        {
+            _registrationNumber = registrationNumber;
+            Console.WriteLine("Vehicle is being initalized. {0}", registrationNumber);
+        }
+    }
+}
