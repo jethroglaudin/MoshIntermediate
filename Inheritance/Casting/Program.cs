a88cf6fb9fd7ab8be0f28f008d430a39cc2a2e36
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-02-11 19:48:21 -0500

    added casting solution

---
+++ Inheritance/Casting/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Casting
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Text text = new Text();
+            Shape shape = text;
+
+            text.Width = 200;
+            shape.Width = 100;
+
+            Console.WriteLine(text.Width);
+        }
+    }
+}
