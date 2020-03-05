95b264e25e4f6f0cc6794b997abd3212ff7de20d
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-04 22:59:29 -0500

    Learning boxing and unboxing

--- Inheritance/BoxingAndUnboxing/Program.cs
+++ Inheritance/BoxingAndUnboxing/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
 
 namespace BoxingAndUnboxing
 {
     class MainClass
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add("Mosh");
+            list.Add(DateTime.Today);
+
+            //var number = (int)list[1];
+
+            var anotherList = new List<int>();
+            //anotherList.Add()
+            var names = new List<string>();
+            names.Add("String");
+
         }
     }
 }
