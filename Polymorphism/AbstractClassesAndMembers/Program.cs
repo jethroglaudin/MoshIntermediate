d46c0d27cdf6501df462a21de9ec6f6f90104c83
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-07 16:47:37 -0500

    working with abstract classes and members

---
+++ Polymorphism/AbstractClassesAndMembers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AbstractClassesAndMembers
+{
+    public class Rectangle: Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a rectangle");
+        }
+    }
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            var circle = new Circle();
+            circle.Draw();
+
+            var rectangle = new Rectangle();
+            rectangle.Draw();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
