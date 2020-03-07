6fcd107b1dbb9b64f9996b35ef46d12886268798
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-06 23:43:30 -0500

    praciting polymorphism

--- Polymorphism/Shape.cs
+++ Polymorphism/Shape.cs
@@ -1,11 +1,32 @@
-﻿namespace Polymorphism
+﻿namespace MethodOverriding
 {
-    class Shape
+   public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a circle");
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a rectangle");
+
+        }
+    }
+   public class Shape
     {
         public int Width { get; set; }
         public int Height { get; set; }
         public Position Position { get; set; }
-        public ShapeType Type { get; set; }
+
+        public virtual void Draw()
+        {
+
+        }
+     
 
     }
 }
