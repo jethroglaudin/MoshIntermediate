1132c44f11dd0bbd882bf4a6c7e39c377bc9f26b
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-08 12:30:36 -0400

    updated main class

--- Polymorphism/AbstractClassesAndMembers/Program.cs
+++ Polymorphism/AbstractClassesAndMembers/Program.cs
@@ -1,24 +1,18 @@
-﻿using System;
-
-namespace AbstractClassesAndMembers
+﻿namespace AbstractClassesAndMembers
 {
-    public class Rectangle: Shape
-    {
-        public override void Draw()
-        {
-            Console.WriteLine("Draw a rectangle");
-        }
-    }
     class MainClass
     {
         static void Main(string[] args)
         {
+            //var shape = new Shape();
+
+
             var circle = new Circle();
             circle.Draw();
 
             var rectangle = new Rectangle();
             rectangle.Draw();
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
