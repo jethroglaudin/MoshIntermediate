520001d8343359341b8b465d4b114056dd529a7b
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-09 18:21:36 -0400

    updated Program.cs

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -6,7 +6,7 @@ namespace Testability
     {
         public static void Main(string[] args)
         {
-            var orderProcessor = new OrderProcessor();
+            var orderProcessor = new OrderProcessor(new ShippingCalculator());
             var order = new Order { DatePlaced = DateTime.Now, TotalPrice = 100f };
             orderProcessor.Process(order);
             Console.WriteLine("Hello World!");
