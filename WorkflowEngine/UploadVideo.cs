29216c719c35eecfc0cd7682749c459945e37b8a
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-09 23:19:49 -0400

    created Upload Video class

---
+++ WorkflowEngine/UploadVideo.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkflowEngine
+{
+    public class UploadVideo : Logger, IInitTask
+    {
+        private string _message = "Uploading User's Video";
+
+        public void Execute()
+        {
+            Log(_message);
+        }
+    }
+}
