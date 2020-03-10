7e3fbce9bf538485d66b0cb35f79bc45251ee107
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-09 23:23:53 -0400

    WorkFlowEngine created

---
+++ WorkflowEngine/WorkFlowEngine.cs
@@ -0,0 +1,14 @@
+﻿namespace WorkflowEngine
+{
+    public class WorkFlowEngine
+    {
+        public void Run(IWorkFlow workflow)
+        {
+            foreach (IInitTask operation in workflow.GetTasks())
+            {
+                operation.Execute();
+            }
+
+        }
+    }
+}
