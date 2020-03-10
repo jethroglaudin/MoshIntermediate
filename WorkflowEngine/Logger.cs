02f41ab00ffad2b83addcb645409a19917b7ec7a
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-09 23:19:07 -0400

    created Logger class

---
+++ WorkflowEngine/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkflowEngine
+{
+    public class Logger
+    {
+        public void Log(String message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
