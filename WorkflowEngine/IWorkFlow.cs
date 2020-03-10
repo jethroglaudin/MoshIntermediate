7a4812f0c0478e3bbe68e45f135b458da20aeb58
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-09 23:26:45 -0400

    created IWorkFlow interface

---
+++ WorkflowEngine/IWorkFlow.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace WorkflowEngine
+{
+    public interface IWorkFlow
+    {
+        void Add(IInitTask task);
+        void Remove(IInitTask task);
+        IEnumerable<IInitTask> GetTasks();
+    }
+}
