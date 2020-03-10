0f84d0210a0cf311301d3dfa1c8dfdcc53a562de
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-09 23:20:11 -0400

    Created WorkFlow class

---
+++ WorkflowEngine/WorkFlow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace WorkflowEngine
+{
+    public class WorkFlow : IWorkFlow
+    {
+        private readonly List<IInitTask> _tasks;
+
+
+        public WorkFlow()
+        {
+            _tasks = new List<IInitTask>();
+        }
+
+        public void Add(IInitTask task)
+        {
+            _tasks.Add(task);
+        }
+
+        public void Remove(IInitTask task)
+        {
+            _tasks.Remove(task);
+        }
+        public IEnumerable<IInitTask> GetTasks()
+        {
+            return _tasks;
+        }
+
+    }
+}
