64f67ad712283fb42399822ef057d80a395b792d
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-09 23:18:19 -0400

    created Change Status class

---
+++ WorkflowEngine/ChangeStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkflowEngine
+{
+    public class ChangeStatus : Logger, IInitTask
+    {
+        private string _message = "Status changed to processing....";
+
+        public void Execute()
+        {
+            Log(_message);
+        }
+    }
+}
