e5d3439a7dd2ffab26fffc5cabc4f99cfdaab2eb
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-09 23:19:27 -0400

    created Web Service class

---
+++ WorkflowEngine/WebService.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkflowEngine
+{
+    public class WebService : Logger, IInitTask
+    {
+        private string _message = "Calling Web service to encode user's video....";
+
+        public void Execute()
+        {
+            Log(_message);
+        }
+    }
+}
