f21970b0f61acc8d4ec159bfe93b60b300a9773f
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-03-09 23:18:40 -0400

    Created Email class

---
+++ WorkflowEngine/Email.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkflowEngine
+{
+    public class Email : Logger, IInitTask
+    {
+        private string _message = "Sending confirmation email....";
+
+        public void Execute()
+        {
+            Log(_message);
+        }
+    }
+}
